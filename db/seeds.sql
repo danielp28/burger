aca7d46e5d8abccc6e76bf3087190924cd112a5d
Daniel Masters <daniel.p.masters@gmail.com>
2019-10-15 14:53:19 -0500

    intial file create for burger

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sunrise Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
+
